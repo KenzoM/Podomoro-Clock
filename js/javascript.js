9710ff2b94f24d0999a16ca0c7de2a8227aa697c
Kenzo Mendoza <kenzom954@gmail.com>
2016-06-14 20:08:43 -0700

    Finished timer. Work on Break timer

--- js/javascript.js
+++ js/javascript.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-  var sessionDefault = 25;
+  var sessionDefault = 1;
   var breakDefault = 5;
   var start = 0;
 
@@ -19,6 +19,9 @@ $(document).ready(function(){
     $("#session").find(".number").text(sessionDefault);
     $("#break").find(".number").text(breakDefault);
     $("#time").find(".number").text(sessionDefault);
+    $("#time").find(".second").text("00");
+    clearInterval(refresh);
+    start = 0;
   })
 
 
@@ -37,19 +40,28 @@ $(document).ready(function(){
   }
 
   $("#session > p").children(".more").click(function(){
+    if (start % 2 == 1){
+      clearInterval(refresh)
+      start --;
+    }
     var currentNumber = $(this).siblings(".number").text();
     $(this).siblings(".number").text(addition(currentNumber));
     $("#time").html('<p>Session Time <span class="number">'+
-      addition(currentNumber) +'</span>:00</p>')
-
+      addition(currentNumber) +'</span>:<span class="second">'+ "00"
+      +'</span></p>')
   })
 
   $("#session > p").children(".less").click(function(){
+    if (start % 2 == 1){
+      clearInterval(refresh)
+      start --;
+    }
     var currentNumber = $(this).siblings(".number").text();
     $(this).siblings(".number").text(subtract(currentNumber));
     if (subtract(currentNumber)){
       $("#time").html('<p>Session Time <span class="number">'+
-        subtract(currentNumber) +'</span>:00</p>')
+        subtract(currentNumber) +'</span>:<span class="second">'+ "00"
+        +'</span></p>')
     }
   })
 
@@ -64,28 +76,39 @@ $(document).ready(function(){
   })
 
   function startTimer(length){
-    console.log(length)
     var minutes = Math.floor(length / 60);
     minutes = (minutes < 10) ? "0" + minutes : minutes;
     var seconds = length % 60;
     seconds = (seconds < 10) ? "0" + seconds : seconds;
     var text = '<p>Session Time <span class="number">'+ minutes
-      +'</span>:'+ seconds +'</p>';
+      +'</span>:<span class="second">'+ seconds +'</span></p>';
     $("#time").html(text);
   }
+
+  function startBreak(breakLength){
+
+  }
+
   var refresh; //this is what will call the timer
   $("#time").click(function(){
     start ++;
-    var timerLength = $(this).find(".number").text() * 60;
+    var jam = $("#time > p").find(".number").text()
+    var ham = $("#time > p").find(".second").text()
+    jam = parseInt(jam) * 60;
+    ham = parseInt(ham) ;
+    var timerLength = (jam + ham)
     if (start % 2 === 1){
       refresh = setInterval(function(){
-        console.log(timerLength)
-        timerLength --;
-        startTimer(timerLength);
+        if (timerLength === 0){
+          alert("Break Time!")
+          clearInterval(refresh);
+        } else {
+          timerLength --;
+          startTimer(timerLength);
+        }
       }, 1000);
     } else{
       clearInterval(refresh);
-      console.log("Stops the timer")
     }
   });
 });
